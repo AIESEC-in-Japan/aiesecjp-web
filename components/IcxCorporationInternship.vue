bb94ec4ee0cf846dd4ce60074119a86c75fa026c
yuyatakahashi <yuya.takahashi@aiesec.jp>
2020-08-16 21:34:08 +0900

    fix indent

--- components/IcxCorporationInternship.vue
+++ components/IcxCorporationInternship.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="corporation-internship-bg">
-    <div class="corporation-internship">
-      <h2>アイセックの企業インターンシップ</h2>
-      <p>42⽇以上、3ヶ⽉未満の短期インターンシップです。</p>
-      <p>豊富な知識とスキルの持った海外の若者をインターン⽣として</p>
-      <p>受け⼊れていただくことで、企業様の課題や挑戦に新たな視点をもたらし、</p>
-      <p>世界を舞台にした発展に寄与いたします。</p>
-    <button class="button">インターンシップの詳細はこちら</button>
-    </div> 
+      <div class="corporation-internship">
+        <h2>アイセックの企業インターンシップ</h2>
+        <p>42⽇以上、3ヶ⽉未満の短期インターンシップです。</p>
+        <p>豊富な知識とスキルの持った海外の若者をインターン⽣として</p>
+        <p>受け⼊れていただくことで、企業様の課題や挑戦に新たな視点をもたらし、</p>
+        <p>世界を舞台にした発展に寄与いたします。</p>
+      <button class="button">インターンシップの詳細はこちら</button>
+      </div> 
     </div>
 </template>
 
@@ -30,21 +30,21 @@
   align-content: space-between;
   justify-content: center;
   text-align: center;
-  h2{
-  color: white;
-  font-size: 24px;
-  padding-top: 80px;
-  padding-bottom: 60px;
-  letter-spacing: 2px;
-  font-family: "游ゴシック";
-  }
-  p{
-  color:white;
-  font-size: 16px;
-  padding: 4px;
-  letter-spacing: 2px;
-  font-family: "游ゴシック";
-  }
+    h2{
+    color: white;
+    font-size: 24px;
+    padding-top: 80px;
+    padding-bottom: 60px;
+    letter-spacing: 2px;
+    font-family: "游ゴシック";
+    }
+    p{
+    color:white;
+    font-size: 16px;
+    padding: 4px;
+    letter-spacing: 2px;
+    font-family: "游ゴシック";
+    }
 }
 .button{
   margin-top: 60px;
