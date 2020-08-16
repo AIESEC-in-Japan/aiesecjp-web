bb5fb22d05d620ccc2654e54c5fe7966caf89960
yuyatakahashi <yuya.takahashi@aiesec.jp>
2020-08-16 20:17:09 +0900

    feature/component icx-corporation-internship

---
+++ pages/icx/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="wrapper">
+   <IcxCorporationInternship />
+  </div>
+</template>
+
+<script>
+import IcxCorporationInternship from '~/components/IcxCorporationInternship.vue'
+
+export default {
+  components: {
+  IcxCorporationInternship,  
+  }
+}
+</script>
+
+<style></style>
