b8bf557ecdb145e9bab69fb1dcfef79622e58f02
yuyatakahashi <yuya.takahashi@aiesec.jp>
2020-08-22 10:26:06 +0900

    feature/component-icx-detail

---
+++ components/IcxDetail.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="detail-container">
+    <div class="detail-bg">
+    <div class="detail">
+    <button class="button1">受け入れ実例をもっと見る</button>
+    <button class="button2">インターンシップの詳細を見る</button>
+    　<p>アイセックの海外インターンシップ受け⼊れに興味のある団体様は、お気軽にお問い合わせください。<br>
+    　   弊団体のメンバーが、疑問解消のお⼿伝いや詳しい⼿続きのご案内をさせていただきます。</p>
+    <button class="button3">話を聞いて見る</button>
+      <p>▶ お問い合わせページに遷移</p>
+    </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.detail-container{
+  width: 100%;
+  height: 100vh;
+  background-image: linear-gradient(rgba(0, 0, 0, .5), rgba(0, 0, 0, .5)), url('../assets/icx-detail.jpg');
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center 70%;
+}
+.detail-bg{
+  width: 100%;
+  height: 50vh;
+  background-color: white;
+}
+.detail{
+  width: 100%;
+  height: 100vh;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  align-content: space-between;
+  justify-content: center;
+  text-align: center;
+    p{
+      color:white;
+      font-size: 14px;
+      padding-top: 50px;
+      padding-bottom: 50px;
+      letter-spacing: 2px;
+      font-family: "游ゴシック";
+    }
+}
+.button1{
+  padding: 15px 90px;
+  color: #0CB9C1;
+  background-color: transparent;
+  border-radius:40px;
+  border: solid 2px;
+  font-weight: bold;
+  font-size: 18px;
+  font-family: "游ゴシック";
+  letter-spacing: 2px;
+  margin-top: 80px;
+}
+.button2{
+  margin-top: 35px;
+  margin-bottom: 140px;
+  padding: 15px 68px;
+  color: #0CB9C1;
+  background-color: transparent;
+  border-radius:40px;
+  border: solid 2px;
+  font-weight: bold;
+  font-size: 18px;
+  font-family: "游ゴシック";
+  letter-spacing: 2px;
+}
+.button3{
+  padding: 15px 90px;
+  color: white;
+  background-color: transparent;
+  border-radius:40px;
+  border: solid 2px;
+  font-weight: bold;
+  font-size: 16px;
+  font-family: "游ゴシック";
+  letter-spacing: 2px;
+}
+
+</style>
--- pages/icx/index.vue
+++ pages/icx/index.vue
@@ -2,17 +2,20 @@
   <div class="wrapper">
    <IcxCorporationInternship />
    <IcxCorporationMerit />
+   <IcxDetail />
   </div>
 </template>
 
 <script>
 import IcxCorporationInternship from '~/components/IcxCorporationInternship.vue'
 import IcxCorporationMerit from '~/components/IcxCorporationMerit.vue'
+import IcxDetail from '~/components/IcxDetail.vue'
 
 export default {
   components: {
   IcxCorporationInternship, 
-  IcxCorporationMerit 
+  IcxCorporationMerit,
+  IcxDetail
   }
 }
 </script>
