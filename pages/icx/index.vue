92f9a903aaf572b74ffeaa24cc4609148a5ee0a0
naoki_mitsuboshi <naoki.mitsuboshi@aiesec.jp>
2020-08-31 18:35:10 +0900

    IcxCorporationVoice の作成

--- pages/icx/index.vue
+++ pages/icx/index.vue
@@ -1,23 +1,26 @@
 <template>
   <div class="wrapper">
-   <IcxCorporationInternship />
-   <IcxCorporationMerit />
-   <IcxDetail />
+    <IcxCorporationInternship />
+    <IcxCorporationMerit />
+    <IcxCorporationVoice />
+    <IcxDetail />
   </div>
 </template>
 
 <script>
-import IcxCorporationInternship from '~/components/IcxCorporationInternship.vue'
-import IcxCorporationMerit from '~/components/IcxCorporationMerit.vue'
-import IcxDetail from '~/components/IcxDetail.vue'
+import IcxCorporationInternship from "~/components/IcxCorporationInternship.vue";
+import IcxCorporationMerit from "~/components/IcxCorporationMerit.vue";
+import IcxCorporationVoice from "~/components/IcxCorporationVoice.vue";
+import IcxDetail from "~/components/IcxDetail.vue";
 
 export default {
   components: {
-  IcxCorporationInternship, 
-  IcxCorporationMerit,
-  IcxDetail
-  }
-}
+    IcxCorporationInternship,
+    IcxCorporationMerit,
+    IcxCorporationVoice,
+    IcxDetail,
+  },
+};
 </script>
 
 <style></style>
