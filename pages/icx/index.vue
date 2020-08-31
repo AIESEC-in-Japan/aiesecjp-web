8e9fbc440e2b96ef1f0735fd4c489ead27dd38ab
naoki_mitsuboshi <naoki.mitsuboshi@aiesec.jp>
2020-08-31 21:58:58 +0900

    Revert "IcxCorporationVoice の作成"
    This reverts commit 92f9a903aaf572b74ffeaa24cc4609148a5ee0a0.

--- pages/icx/index.vue
+++ pages/icx/index.vue
@@ -1,26 +1,23 @@
 <template>
   <div class="wrapper">
-    <IcxCorporationInternship />
-    <IcxCorporationMerit />
-    <IcxCorporationVoice />
-    <IcxDetail />
+   <IcxCorporationInternship />
+   <IcxCorporationMerit />
+   <IcxDetail />
   </div>
 </template>
 
 <script>
-import IcxCorporationInternship from "~/components/IcxCorporationInternship.vue";
-import IcxCorporationMerit from "~/components/IcxCorporationMerit.vue";
-import IcxCorporationVoice from "~/components/IcxCorporationVoice.vue";
-import IcxDetail from "~/components/IcxDetail.vue";
+import IcxCorporationInternship from '~/components/IcxCorporationInternship.vue'
+import IcxCorporationMerit from '~/components/IcxCorporationMerit.vue'
+import IcxDetail from '~/components/IcxDetail.vue'
 
 export default {
   components: {
-    IcxCorporationInternship,
-    IcxCorporationMerit,
-    IcxCorporationVoice,
-    IcxDetail,
-  },
-};
+  IcxCorporationInternship, 
+  IcxCorporationMerit,
+  IcxDetail
+  }
+}
 </script>
 
 <style></style>
