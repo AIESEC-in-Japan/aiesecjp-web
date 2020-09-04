87bfc65fa8560f5a5836887c5fb75174c798e75f
watanabe chiaki <3435academianuts@gmail.com>
2020-09-04 18:10:59 +0900

    festure IcxTopcomponent

---
+++ components/IcxTopcomponent.vue
@@ -0,0 +1,48 @@
+<template>
+ <div class="top-component">
+  <img src="https://drive.google.com/file/d/13m4V5FvxbQeE_u6FfBa_mtB7FijYXGOe/view?usp=sharing">
+  <div class="top-component-container">
+   <h1>
+      Globalize your company with AIESEC
+   </h1>
+   <h2>
+    　アイセックのインターンシップで、グローバルな組織作りを
+   </h2>
+  </div>
+ </div>
+</template>
+
+
+<style scoped lang="scss">
+ .top-component{
+     max-width: 1000px;
+     filter: brightness(50%);
+     letter-spacing: 2px;
+     background-image: "https://drive.google.com/file/d/1qr8apaNtmJQJKbUECzfA4S7vz-Y5_v7E/view?usp=sharing";
+     background-position: center;
+     background-repeat: none;
+     background-size:cover;    
+ }
+ img{
+     margin: 20px 20px;
+ }
+ .top-component-container{
+      height: 200px;
+     display: flex;
+     justify-content: center;
+     color: #fff;
+
+ }
+ h1{
+     font-family: "Lato";
+     font-size: 40px;
+     display: block;
+     
+ }
+ h2{
+     font-family: "游ゴシック";
+     font-size: 20px;
+     display: block;
+ }
+
+</style>
