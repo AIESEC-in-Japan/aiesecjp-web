aad05d99a2fd75edfc9c4949dfcdbe7ce5a5c543
OMIMAN <takaomi.matsumoto@aiesec.jp>
2020-09-06 15:17:44 +0900

    feature OgxDetail

---
+++ pages/ogx/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="wrapper">
+    <OgxDetail/>
+  </div>
+</template>
+
+<script>
+import OgxDetail from '~/components/OgxDetail.vue'
+
+export default {
+  components: {
+  OgxDetail
+  }
+}
+</script>
+
+<style></style>
