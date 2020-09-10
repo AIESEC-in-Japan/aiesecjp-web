accde9420b73e3eb71fb32fb64aa3e36ecb72f86
yunosuke <yunosuke.h13@gmail.com>
2020-09-10 16:52:20 +0900

    update readme

---
+++ pages/ogx/index.vue
@@ -0,0 +1,39 @@
+<template>
+    
+    <div class="ogxcomponent-top">
+        <div class="ogxcomponent-top-container">
+            <h1>Experience the world with AIESEC</h1>
+            <h2>アイセックの海外インターンシップで、<br>
+                きっと、変わる。自分も、世界も。</h2>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.ogxcomponent-top{
+    height: 800px;
+    letter-spacing: 2px;
+    background-image: url(img/ogx_background.jpg);
+    background-position: center;
+    background-repeat: none;
+    background-size: cover;
+.ogxcomponent-top-container{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    color: #fff;
+h1{
+    text-align: center;
+    font-style: oblique;
+    font-family:"Lato";
+    font-size: 40px;
+}
+h2{
+    text-align: center;
+    font-family: "游ゴシック";
+    font-size: 20px;
+}
+}
+}
+</style>
