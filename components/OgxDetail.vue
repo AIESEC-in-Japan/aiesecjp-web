02a0f78e92e126157bff0a25b7b9faa2a1dca43f
OMIMAN <takaomi.matsumoto@aiesec.jp>
2020-09-11 02:03:10 +0900

    fix/ogx-detail

--- components/OgxDetail.vue
+++ components/OgxDetail.vue
@@ -1,77 +1,87 @@
 <template>
-<div class="ogxcomponent-container-bg">
- <div class="ogxcomponent-main">
-   <div class="ogxcomponent-container">
-     <img src="../assets/product_GV-horizontal-white.png"  height="65px" width="260px">
-     <p>
-     期間：6～8週間<br>
-     インターン先：NPO/NGOなど<br>
-     給与・滞在補助費：無し<br><br><br>
-     貧困、飢餓、健康、養育、格差、戦争、<br>
-     世界は様々な社会課題で溢れています。<br>
-     社会課題の最前線で、課題を五感で<br>
-     捉え、考え、行動し、試行錯誤する。<br>
-     世界に挑戦するための、第一歩</p>
-     <p class="navigation">詳しく見る</p>
-   </div>
+<div class="bg">
+ <div class="container">
+  <div class="main">
+   <img src="../assets/product_GV-horizontal-white.png">
+   <p>
+    期間：6～8週間<br>
+    インターン先：NPO/NGOなど<br>
+    給与・滞在補助費：無し<br>
+   </p>
+   <p>
+    貧困、飢餓、健康、養育、格差、戦争。<br>
+    世界は様々な社会課題で溢れています。<br>
+    社会課題の最前線で、課題を五感で<br>
+    捉え、考え、行動し、試行錯誤する。<br>
+    世界に挑戦するための、第一歩。</p>
+   <a>詳しく見る</a>
+  </div>
  
-   <div class="ogxcomponent-container">
-     <img src="../assets/product_GT-horizontal-color (2).png" height="65px" width="260px">
-     <p>
-     期間：6～7.8週間<br>
-     インターン先：海外企業・ベンチャーなど<br>
-     給与・滞在補助費：9週間以上の場合はあり<br><br><br>
-     世界を前進させるための<br>
-     ビジネススキルや専門知識を身につけ、<br>
-     セクターを超えた社会課題を、<br>
-     世界で通用する専門性を武器に、<br>
-     社会を変えるリーダーへ</p>
-     <p class="navigation">詳しく見る</p>
-   </div>
+  <div class="main">
+   <img src="../assets/product_GT-horizontal-white.png">
+   <p>
+    期間：6～7.8週間<br>
+    インターン先：海外企業・ベンチャーなど<br>
+    給与・滞在補助費：9週間以上の場合はあり<br>
+   </p>
+   <p>
+    世界を前進させるための<br>
+    ビジネススキルや専門知識を身につけ、<br>
+    セクターを超えた課題解決を。<br>
+    世界で通用する専門性を武器に、<br>
+    社会を変えるリーダーへ。</p>
+   <a>詳しく見る</a>
+  </div>
  </div>
 </div>
 </template>
 
 <style scoped lang="scss">
-  .ogxcomponent-container-bg{
+  .bg{
     height: 100vh;
     width: 100%;
     background-image:url('../assets/icx-corporation-internship.jpg');
     background-repeat: no-repeat;
-    background-size: 140%;
+    background-size: cover;
     background-position: 50% 55%;
   }
-
-  .ogxcomponent-main{
+  .container{
     display:flex;
     justify-content:center;
-    width: 100%;
-    height:100vh;
     background-color:rgba(0,0,0,0.5);
   }
-
-  .ogxcomponent-container{   
-    width:30%;
+  img{
+   height:65px; width:230px;
+  }
+  
+  
+  .main{ 
+    padding-right: 40px;
+    padding-left: 40px;
     height: 100vh;
     text-align: center;
     padding-top: 190px;
     
-     p{
-       margin-top: 50px;
-       font-size: 12px;
-       font-weight:bold;
-       color: white;
-       font-family: "游ゴシック";
-       }
-        .navigation {
-         margin-top: 40px;
-         font-weight: normal;
-         font-size: 16px;
-       }
+    p{
+      margin-top: 40px;
+      font-size: 14px;
+      font-weight:bold;
+      color: white;
+      font-family: "游ゴシック";
+    }
+    
+    a{
+     margin-top:40px;
+     display: inline-block;
+     font-weight: normal;
+     font-size: 16px;
+     color: white;
+     border-bottom:solid 1.5px;
+    }
   }
   
-  
 
+  
 </style>
 
   
--- pages/ogx/index.vue
+++ pages/ogx/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="wrapper">
-    <OgxDetail/>
+    <OgxDetail />
   </div>
 </template>
 
