aac3e0b3767861840498a93848bd4e6879990520
naoki_mitsuboshi <naoki.mitsuboshi@aiesec.jp>
2020-09-11 18:10:14 +0900

    クラス名の変更、色を変数に変更、letter-spacingの設定、フォントファミリーの変更

--- components/IcxCorporationVoice.vue
+++ components/IcxCorporationVoice.vue
@@ -1,72 +1,83 @@
 <template>
-  <div class="IcxCorporationVoice-container">
-    <div class="IcxCorporationVoice-title">
+  <div class="icxcorporationvoice-container">
+    <div class="icxcorporationvoice-title">
       <h2>インターン⽣受け⼊れ企業様の声</h2>
     </div>
-    <div class="IcxCorporationVoice-content">
-      <div class="IcxCorporationVoice-content-detail">
+    <div class="icxcorporationvoice-content">
+      <div class="icxcorporationvoice-content-detail">
         <img
           src="https://4.bp.blogspot.com/-aJjlevJyK9U/XGjx40QThrI/AAAAAAABRco/j9aRPnYHpX4PU6RZjhTWRh6_8xnPfbNEQCLcBGAs/s400/animal_chara_computer_azarashi.png"
           alt
         />
-        <p>企業名</p>
+        <p class="icxcorporationvoice-content-detail-name">企業名</p>
         <p>インターンの概要</p>
         <p>インターンの概要</p>
         <p>インターンの概要</p>
+        <br />
       </div>
-      <div class="IcxCorporationVoice-content-detail">
+      <div class="icxcorporationvoice-content-detail">
         <img
           src="https://4.bp.blogspot.com/-aJjlevJyK9U/XGjx40QThrI/AAAAAAABRco/j9aRPnYHpX4PU6RZjhTWRh6_8xnPfbNEQCLcBGAs/s400/animal_chara_computer_azarashi.png"
           alt
         />
-        <p>企業名</p>
+        <p class="icxcorporationvoice-content-detail-name">企業名</p>
         <p>インターンの概要</p>
         <p>インターンの概要</p>
         <p>インターンの概要</p>
+        <br />
       </div>
-      <div class="IcxCorporationVoice-content-detail">
+      <div class="icxcorporationvoice-content-detail">
         <img
           src="https://4.bp.blogspot.com/-aJjlevJyK9U/XGjx40QThrI/AAAAAAABRco/j9aRPnYHpX4PU6RZjhTWRh6_8xnPfbNEQCLcBGAs/s400/animal_chara_computer_azarashi.png"
           alt
         />
-        <p>企業名</p>
+        <p class="icxcorporationvoice-content-detail-name">企業名</p>
         <p>インターンの概要</p>
         <p>インターンの概要</p>
         <p>インターンの概要</p>
+        <br />
       </div>
     </div>
   </div>
 </template>
 
 <style scoped lang="scss">
-.IcxCorporationVoice-container {
+.icxcorporationvoice-container {
   width: 100%;
   height: 100vh;
-  .IcxCorporationVoice-title {
+  .icxcorporationvoice-title {
     display: flex;
     justify-content: center;
     h2 {
-      color: #0cb9c1;
+      color: $mint;
       font-size: 24px;
       letter-spacing: 2px;
-      font-family: "Lato";
+      font-family: "游ゴシック", "Yu Gothic"; //for win
+      font-family: "游ゴシック体", YuGothic; //for mac
       padding-top: 150px;
     }
   }
 }
-.IcxCorporationVoice-content {
+.icxcorporationvoice-content {
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
   padding-top: 100px;
-  .IcxCorporationVoice-content-detail {
+  .icxcorporationvoice-content-detail {
     p {
       text-align: center;
+      font-size: 15px;
+      letter-spacing: 2px;
+      font-family: "游ゴシック", "Yu Gothic"; //for win
+      font-family: "游ゴシック体", YuGothic; //for mac
     }
     img {
       width: 350px;
       height: 300px;
       padding: 30px;
+    }
+    .icxcorporationvoice-content-detail-name {
+      font-weight: bold;
     }
   }
 }
