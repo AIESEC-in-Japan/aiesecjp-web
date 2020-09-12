da5df93899dd904a621c3a382773998f8eaf21dd
watanabe chiaki <3435academianuts@gmail.com>
2020-09-12 11:46:10 +0900

    fix style

--- components/IcxTopcomponent.vue
+++ components/IcxTopcomponent.vue
@@ -1,51 +1,60 @@
 <template>
  <div class="icx-top-container">
-  <img src="../assets/product_GT-horizontal-white.png">
-  <h1>
-    Globalize your company with AIESEC
-  </h1>
-  <h2>
-    アイセックのインターンシップで、グローバルな組織作りを
-  </h2>
+   <div class="icx-top">
+      <img src="../assets/product_GT-horizontal-white.png" alt="GT-logo">
+      <h1>
+        Globalize your company with AIESEC
+      </h1>
+      <span>
+        アイセックのインターンシップで、グローバルな組織作りを
+      </span>
+   </div>
  </div>
 </template>
 
 
 <style scoped lang="scss">
- .icx-top-container{
+ .icx-top-container {
      width: 100%;
-     filter: brightness(55%) saturate(120%);
      background-image: url('../assets/icx-top.jpg');
      background-position: center;
      background-repeat: none;
      background-size:cover;  
      height: 100vh;
-     display: flex;
-     justify-content: center;
-     flex-direction: column;
-     align-items: center;
  }
- img{
-     margin: 18px 18px;
+ .icx-top {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+    background-color: rgba(0,0,0,5);
+ }
+
+ img {
+     margin-bottom: 24px;
+     margin-left: 18px;
+     margin-right: 18px;
+     margin-top: 18px;
      display: flex;
      width: 400px;
  }
 
- h1{
+ h1 {
      font-family: "Lato";
-     font-size: 20px;
+     font-size: 24px;
      font-style: oblique;
      display: flex;
      letter-spacing: 1px;
      color: white;  
  }
 
- h2{
+ span {
      font-family: "游ゴシック";
      font-size: 10px;
      display: flex;
      letter-spacing: 5px;
-     color: white;  
+     color: white; 
+     font-weight: lighter; 
  }
 
 </style>
