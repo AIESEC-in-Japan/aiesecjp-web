217d6659daabeef66bff60c419b312fa6e5301e7
yunosuke <yunosuke.h13@gmail.com>
2020-09-15 12:43:35 +0900

    feature/ogx top

---
+++ components/OgxTop.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="container">
+    <div class="ogx-top">
+      <h1 class="title">Experience the world with AIESEC</h1>
+      <p class="catchcopy">
+        <span>アイセックの海外インターンシップで、</span>
+        <span>きっと、変わる。自分も、世界も。</span>
+      </p>
+    </div>
+  </div>
+</template>
+<style scoped lang="scss">
+.container {
+  background-image: url("../assets/ogx_top_background.jpg");
+  background-position: center;
+  background-repeat: none;
+  background-size: cover;
+}
+.ogx-top {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  color: #fff;
+  height: 100vh;
+  letter-spacing: 2px;
+  background-color: rgba(0, 0, 0, 0.4);
+  .title {
+    text-align: center;
+    font-style: oblique;
+    letter-spacing: 0px;
+    font-family:"Lato";
+    font-size: 24px;
+    margin-bottom: 18px;
+  }
+  .catchcopy {
+    text-align: center;
+    font-family: "游ゴシック";
+    font-size: 16px;
+    letter-spacing: 3px;
+    span {
+      display: block;
+    }
+  }
+}
+</style>
--- pages/outgoing/index.vue
+++ pages/outgoing/index.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="wrapper">
+    <OgxTop />
     <OgxDetail />
   </div>
 </template>
 
 <script>
+import OgxTop from '~/components/OgxTop.vue'
 import OgxDetail from '~/components/OgxDetail.vue'
 
 export default {
   components: {
+    OgxTop,
     OgxDetail
   }
 }
