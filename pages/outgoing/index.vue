1e5eade28ea3bc6208b05918702d9ca9bd0725b2
yunosuke <yunosuke.h13@gmail.com>
2020-09-16 13:08:37 +0900

    fix index.vue

--- pages/outgoing/index.vue
+++ pages/outgoing/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="wrapper">
+    <OgxTop />
     <OgxGVGTDetail />
     <OgxDetail />
   </div>
@@ -7,11 +8,13 @@
 
 <script>
 
+import OgxTop from '~/components/OgxTop.vue'
 import OgxGVGTDetail from '~/components/OgxGVGTDetail.vue'
 import OgxDetail from '~/components/OgxDetail.vue'
 
 export default {
   components: {
+    OgxTop,
     OgxGVGTDetail,
     OgxDetail
   }
