6c9c1970f8f0f520e6dbc8727b5c23793cba06a9
watanabe chiaki <3435academianuts@gmail.com>
2020-09-17 22:25:18 +0900

    fix style

---
+++ pages/about/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="wrapper">
+        <OrganizationInfo />
+    </div>
+</template>
+
+<script>
+import OrganizationInfo from '~/components/OrganizationInfo.vue'
+
+export default {
+    components: {
+        OrganizationInfo
+    }
+}
+</script>
