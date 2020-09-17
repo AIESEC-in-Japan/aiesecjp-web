cc5c996599faf89e27f28008fd58c897b5407206
OMIMAN <takaomi.matsumoto@aiesec.jp>
2020-09-17 12:21:37 +0900

    spanの導入など

--- components/OgxFeature.vue
+++ components/OgxFeature.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="component">
+  <div class="container">
     <h1>アイセックの海外インターンシップの特徴</h1>
-    <div class ="content">   
+    <div class ="feature">   
       <div class="detail">  
         <img src="https://placehold.jp/240x240.png" alt="" class="partners-content">
         <h2>
@@ -9,11 +9,11 @@
           世界への挑戦機会
         </h2>  
         <p>
-          6週間の間、参加者は現地に存在する社会課題<br>
-          や企業課題に自らアプローチします。異国の<br>
-          地という挑戦的な環境において実戦的な経験<br>
-          を積むことによって、日本では得られない、<br>
-          密度の高い成長経験を提供します。
+          <span>6週間の間、参加者は現地に存在する社会課題</span>
+          <span>や企業課題に自らアプローチします。異国の</span>
+          <span>地という挑戦的な環境において実戦的な経験</span>
+          <span>を積むことによって、日本では得られない、</span>
+          <span>密度の高い成長経験を提供します。</span>
         </p>
       </div>
       <div class="detail">  
@@ -23,11 +23,11 @@
           丁寧なサポート
         </h2>
         <p>
-         　参加者には問おう先選定時から渡航中、渡航後<br>
-    　　 　まで、インターン生の皆様を専属マネー<br>
-      　 　ジャーがサポートします。渡航前の準備や渡<br>
-         　航中の壁打ち、渡航後の振り返りなど、参加<br>
-         　者の成長を二人三脚で最大化します。
+         　<span>参加者には問おう先選定時から渡航中、渡航後</span>
+    　　 　<span>まで、インターン生の皆様を専属マネー</span>
+      　 　<span>ジャーがサポートします。渡航前の準備や渡</span>
+         　<span>航中の壁打ち、渡航後の振り返りなど、参加</span>
+         　<span>者の成長を二人三脚で最大化します。</span>
         </p>
       </div>
       <div class="detail">  
@@ -37,11 +37,11 @@
         事前・事後プログラム
       </h2>
       <p>
-        渡航前には成長計画の明確化や行動設計を行<br>
-        い、渡航後は学びの言語化などを通じて、<br>
-        「渡航しただけ」で終わらない機会を提供し<br>
-        ます。パートナー企業様と共同開催の事前事<br>
-        後のプログラムもご用意しております。
+        <span>渡航前には成長計画の明確化や行動設計を行</span>
+        <span>い、渡航後は学びの言語化などを通じて、</span>
+        <span>「渡航しただけ」で終わらない機会を提供し</span>
+        <span>ます。パートナー企業様と共同開催の事前事</span>
+        <span>後のプログラムもご用意しております。</span>
       </p>
       </div>
     </div>
@@ -49,7 +49,7 @@
 </template>
 
 <style scoped lang="scss">
-.component  {
+.container {
     h1  {
       display: flex;
       justify-content: center;
@@ -61,13 +61,17 @@
       font-size: 24px;
     }
 }
-.content  {
+.feature  {
     display: flex;
+    flex-wrap: wrap;
     justify-content: center;
 }
 .detail  {
   text-align: center;
   padding: 20px;
+  img{
+    border-radius: 30px;
+  }
   h2  {
       font-size: 16px;
       line-height: 30px;
@@ -82,8 +86,11 @@
     letter-spacing: 2px;
     line-height: 20px;
     font-family: "Lato";
-    font-weight: bolder;
     margin-bottom: 50px;
+
+    span{
+      display: block;
+    }
   }
 }
 </style>
