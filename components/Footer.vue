747ed03cf534b230b80c962d36361a7f04087c02
sgz-cal <ls16012j@gmail.com>
2020-09-21 16:48:54 +0900

    展開アニメーションの実装

--- components/Footer.vue
+++ components/Footer.vue
@@ -81,88 +81,98 @@
       <button type="button" class="sp-footer-title" :class="{ '_state-open': isOpened[0] }" @click="openItems(0)">
         AIESECについて
       </button>
-      <div class="sp-footer-item" :class="{ '_state-open': isOpened[0] }" v-if="isOpened[0]">
-        <p>AIESEC(アイセック)は、カナダのモントリオールに本部を置く海外インターンシップの運営を主幹事業とする世界最大級の学生団体です。アイセックの日本支部であるアイセック・ジャパンは1962年に創設され、現在では主に大学ごとに25の委員会が活動をしています。</p>
-      </div>
+      <transition name="sp-footer-item" @before-enter="beforeEnter" @enter="enter" @before-leave="beforeLeave" @leave="leave">
+        <div class="sp-footer-item" :class="{ '_state-open': isOpened[0] }" v-if="isOpened[0]">
+          <p>AIESEC(アイセック)は、カナダのモントリオールに本部を置く海外インターンシップの運営を主幹事業とする世界最大級の学生団体です。アイセックの日本支部であるアイセック・ジャパンは1962年に創設され、現在では主に大学ごとに25の委員会が活動をしています。</p>
+        </div>
+      </transition>
     </div>
     <div class="sp-footer-content">
       <button type="button" class="sp-footer-title" :class="{ '_state-open': isOpened[1] }" @click="openItems(1)">
         About
       </button>
-      <ul class="sp-footer-item" :class="{ '_state-open': isOpened[1] }" v-if="isOpened[1]">
-        <li>
-          <a href="#">AIESECについて</a>
-        </li>
-        <li>
-          <a href="#">パートナーシップについて</a>
-        </li>
-        <li>
-          <a href="#">団体情報</a>
-        </li>
-        <li>
-          <a href="#">安全への取り組み</a>
-        </li>
-        <li>
-          <a href="#">個人情報の取り扱い</a>
-        </li>
-        <li>
-          <a href="#">お問い合わせ</a>
-        </li>
-      </ul>
+      <transition name="sp-footer-item" @before-enter="beforeEnter" @enter="enter" @before-leave="beforeLeave" @leave="leave">
+        <ul class="sp-footer-item" :class="{ '_state-open': isOpened[1] }" v-if="isOpened[1]">
+          <li>
+            <a href="#">AIESECについて</a>
+          </li>
+          <li>
+            <a href="#">パートナーシップについて</a>
+          </li>
+          <li>
+            <a href="#">団体情報</a>
+          </li>
+          <li>
+            <a href="#">安全への取り組み</a>
+          </li>
+          <li>
+            <a href="#">個人情報の取り扱い</a>
+          </li>
+          <li>
+            <a href="#">お問い合わせ</a>
+          </li>
+        </ul>
+      </transition>
     </div>
     <div class="sp-footer-content">
       <button type="button" class="sp-footer-title" :class="{ '_state-open': isOpened[2] }" @click="openItems(2)">
         For Students
       </button>
-      <ul class="sp-footer-item" :class="{ '_state-open': isOpened[2] }" v-if="isOpened[2]">
-        <li>
-          <a href="#">海外インターンシップに参加する</a>
-        </li>
-        <li>
-          <a href="#">海外ボランティアに参加する</a>
-        </li>
-        <li>
-          <a href="#">体験記を見る</a>
-        </li>
-        <li>
-          <a href="#">参加の流れと費用</a>
-        </li>
-        <li>
-          <a href="#">危機管理サポート</a>
-        </li>
-        <li>
-          <a href="#">話を聞いてみる</a>
-        </li>
-      </ul>
+      <transition name="sp-footer-item" @before-enter="beforeEnter" @enter="enter" @before-leave="beforeLeave" @leave="leave">
+        <ul class="sp-footer-item" :class="{ '_state-open': isOpened[2] }" v-if="isOpened[2]">
+          <li>
+            <a href="#">海外インターンシップに参加する</a>
+          </li>
+          <li>
+            <a href="#">海外ボランティアに参加する</a>
+          </li>
+          <li>
+            <a href="#">体験記を見る</a>
+          </li>
+          <li>
+            <a href="#">参加の流れと費用</a>
+          </li>
+          <li>
+            <a href="#">危機管理サポート</a>
+          </li>
+          <li>
+            <a href="#">話を聞いてみる</a>
+          </li>
+        </ul>
+      </transition>
     </div>
     <div class="sp-footer-content">
       <button type="button" class="sp-footer-title" :class="{ '_state-open': isOpened[3] }" @click="openItems(3)">
         For Companies
       </button>
-      <ul class="sp-footer-item" :class="{ '_state-open': isOpened[3] }" v-if="isOpened[3]">
-        <li>
-          <a href="#">海外インターン生を受け入れる</a>
-        </li>
-        <li>
-          <a href="#">受け入れ事例を見る</a>
-        </li>
-        <li>
-          <a href="#">受け入れの流れと費用</a>
-        </li>
-      </ul>
+      <transition name="sp-footer-item" @before-enter="beforeEnter" @enter="enter" @before-leave="beforeLeave" @leave="leave">
+        <ul class="sp-footer-item" :class="{ '_state-open': isOpened[3] }" v-if="isOpened[3]">
+          <li>
+            <a href="#">海外インターン生を受け入れる</a>
+          </li>
+          <li>
+            <a href="#">受け入れ事例を見る</a>
+          </li>
+          <li>
+            <a href="#">受け入れの流れと費用</a>
+          </li>
+        </ul>
+      </transition>
     </div>
     <div class="sp-footer-content">
       <button type="button" class="sp-footer-title" :class="{ '_state-open': isOpened[4] }" @click="openItems(4)">
         Press & Alumni
       </button>
-      <ul class="sp-footer-item" :class="{ '_state-open': isOpened[4] }" v-if="isOpened[4]">
-        <li>
-          <a href="#">プレスルーム</a>
-        </li>
-        <li>
-          <a href="#">アルムナイの方</a>
-        </li>
-      </ul>
+      <transition name="sp-footer-item" @before-enter="beforeEnter" @enter="enter" @before-leave="beforeLeave" @leave="leave">
+        <ul class="sp-footer-item" :class="{ '_state-open': isOpened[4] }" v-if="isOpened[4]">
+          <li>
+            <a href="#">プレスルーム</a>
+          </li>
+          <li>
+            <a href="#">アルムナイの方</a>
+          </li>
+        </ul>
+      </transition>
     </div>
   </div>
 </template>
@@ -181,6 +191,18 @@ export default {
     openItems: function(index) {
       this.isOpened.splice(index,1,!this.isOpened[index]);
     },
+    beforeEnter: function(el) {
+      el.style.height = '0';
+    },
+    enter: function(el) {
+      el.style.height = el.scrollHeight + 'px';
+    },
+    beforeLeave: function(el) {
+      el.style.height = el.scrollHeight + 'px';
+    },
+    leave: function(el) {
+      el.style.height = '0';
+  }
   }
 }
 </script>
@@ -239,20 +261,67 @@ export default {
 .sp-footer-title {
   width: 100%;
   font-size: 16px;
+  line-height: 16px;
   color: $blue;
   letter-spacing: 2px;
   padding: 20px;
+}
+.sp-footer-title::after {
+  content: "+";
+  font-size: 24px;
+  float: right;
+  -moz-transition: -moz-transform 0.3s ease-in-out;
+  -o-transition: -o-transform 0.3s ease-in-out;
+  -webkit-transition: -webkit-transform 0.3s ease-in-out;
+  transition: transform 0.3s ease-in-out;
+}
+._state-open::after {
+  -webkit-transform: rotate(45deg);
+  -moz-transform: rotate(45deg);
+  transform: rotate(45deg);
 }
 .sp-footer-item {
   color: $dark-gray;
   width: 80%;
   letter-spacing: 2px;
   line-height: 24px;
+  font-size: 12px;
+  overflow: hidden;
+  transition: height 0.4s ease-in-out;
   a {
     color: $dark-gray;
     text-decoration: none;
     display: block;
     margin: 10px auto;
   }
+  &-enter-active{
+    animation-duration: 1s;
+    animation-fill-mode: both;
+    animation-name: sp-footer-item--anime__opend;
+  }
+  &-leave-active{
+    animation-duration: 1s;
+    animation-fill-mode: both;
+    animation-name: sp-footer-item--anime__closed;
+  }
+}
+@keyframes sp-footer-item--anime__opend {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+   }
+}
+@keyframes sp-footer-item--anime__closed {
+  0% {
+   opacity: 1;
+  }
+  100% {
+    opacity: 0;
+  }
+}
+._state-open {
+  max-height: 500px;
 }
 </style>
