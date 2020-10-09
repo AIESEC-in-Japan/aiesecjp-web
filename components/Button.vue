9f92f9032cd82920cb413b24685e4bf67465d0c6
OMIMAN <takaomi.matsumoto@aiesec.jp>
2020-10-10 00:42:51 +0900

    コンポーネントの修正

--- components/Button.vue
+++ components/Button.vue
@@ -1,21 +1,22 @@
 <template>
-  <button>{{name}}</button>
+  <button :style="{ width: width,color:color }">{{name}}</button>
 </template>
 
 <script>
- export default {
+export default {
   props: {
     name: String,
+    color: String,
+    width: String
   }
- };
+};
 </script>
 
 <style scoped lang="scss">
-  button {
+button {
   margin-top: 35px;
   margin-bottom: 140px;
   padding: 15px;
-  color: $mint;
   background-color: transparent;
   border-radius: 40px;
   border: solid 2px;
--- pages/outgoing/index.vue
+++ pages/outgoing/index.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="wrapper">
     <Top />
-    <Button :name="buttonName" />
     <Feature />
     <GVGTDetail />
     <Detail />
@@ -11,7 +10,6 @@
 
 <script>
 import Top from '@/components/outgoing/Top.vue'
-import Button from '@/components/Button.vue'
 import Feature from '@/components/outgoing/Feature.vue'
 import GVGTDetail from '@/components/outgoing/GVGTDetail.vue'
 import Detail from '@/components/outgoing/Detail.vue'
@@ -20,16 +18,10 @@ import InternshipExperience from '@/components/outgoing/InternshipExperience.vue
 export default {
   components: {
     Top,
-    Button,
     Feature,
     GVGTDetail,
     Detail,
     InternshipExperience
-  },
-  data() {
-    return {
-      buttonName: "Omiman",
-    };
   }
 };
 
