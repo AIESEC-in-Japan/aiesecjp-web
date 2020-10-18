3bf5ab8c99346de85901535a8cc4102916c440e4
watanabe chiaki <3435academianuts@gmail.com>
2020-10-18 12:03:08 +0900

    fix comentout

---
+++ components/base/Button.vue
@@ -0,0 +1,32 @@
+
+<template>
+  <a href="http://www.aiesec.jp/" class="Button">新歓説明会に行ってみる</a>
+</template>
+
+
+
+<style scoped lang="scss">
+.Button {
+  display: block;
+  padding-top: 15px;
+  padding-bottom: 15px;
+  width: 350px;
+  font-size: 16px;
+  font-weight: bold;
+  letter-spacing: 5px;
+  background-color: white;
+  color: #00aeef;
+  -webkit-transition: all 0.3s ease;
+  -moz-transition: all 0.3s ease;
+  -o-transition: all 0.3s ease;
+  transition: all  0.3s ease;
+  border: solid 2px #00aeef;
+  border-radius:15px;
+  text-align: center;
+  text-decoration: none;
+  &:hover {
+    background-color: #00aeef;
+    color: white;       
+  }
+}
+</style>
