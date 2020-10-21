a6107e8e1f212624b0ccd3ca9521e8ddbf2adb38
OMIMAN <takaomi.matsumoto@aiesec.jp>
2020-10-21 14:21:39 +0900

    現時点でのコミット

--- components/incoming/Detail.vue
+++ components/incoming/Detail.vue
@@ -2,7 +2,7 @@
   <div class="detail-container">
     <div class="detail-bg">
       <div class="detail">
-        <button href="#" class="button-examples">受け入れ実例をもっと見る</button>
+        <button href="#" name="そんなこと" color="#00CCCC" width="200px"></button>
         <button href="#" class="button-detail">インターンシップの詳細を見る</button>
         <p>
           <span>アイセックの海外インターンシップ受け⼊れに</span><span>興味のある団体様は、</span><span>お気軽にお問い合わせください。</span><span>弊団体のメンバーが、疑問解消のお⼿伝いや詳しい⼿続きの</span><span>ご案内をさせていただきます。</span>
@@ -13,9 +13,20 @@
   </div>
 </template>
 
+<script>
+import Button from '@/components/base/Button.vue'
+
+export default {
+  props: {
+    name: String,
+    color: String,
+    width: String
+  }
+};
+</script>
+
 <style scoped lang="scss">
 .detail-container {
-  width: 100%;
   height: 100vh;
   background-image: linear-gradient(rgba(0, 0, 0, .5), rgba(0, 0, 0, .5)), url("~@/assets/images/icx-detail.jpg");
   background-repeat: no-repeat;
@@ -51,7 +62,6 @@
 }
 .button-examples {
   padding: 15px 90px;
-  color: $mint;
   background-color: transparent;
   border-radius: 40px;
   border: solid 2px;
