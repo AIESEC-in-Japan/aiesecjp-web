fced6795040050c087b9eb6a49db0774009828f2
yuki komatsu <yuki.k.lion@gmail.com>
2021-05-02 18:27:36 +0900

    refactor and replace movie

--- components/top/MainView.vue
+++ components/top/MainView.vue
@@ -1,29 +1,21 @@
 <template>
   <div class="main-view-container">
     <div class="pattern"></div>
-<!--    FIXME: 動画差し替え -->
-      <video autoplay loop muted src="http://webcreatorbox.com/sample/images/jewellery.mov">
-        <img alt="Placeholder" src="http://webcreatorbox.com/sample/images/jewellery.jpg">
-      </video>
+    <video autoplay loop muted src="~@/assets/videos/withAiesecVideo.mp4">
+      <!--    FIXME: 動画が再生されなかった場合の画像置いておく,スマホの場合の対応など -->
+    </video>
   </div>
 </template>
 
-<script>
-export default {
-  name: "mainView"
-}
-</script>
-
 <style lang="scss" scoped>
-.main-view-container{
+.main-view-container {
   height: 100vh;
 }
-
 
 .pattern {
   width: 100%;
   height: 100vh;
-  background: rgba(0,0,0,0.4);
+  background: rgba(0, 0, 0, 0.4);
   position: absolute;
   top: 0;
   left: 0;
