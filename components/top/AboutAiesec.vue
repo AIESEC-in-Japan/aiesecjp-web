6b758107dcd0dec2f245285f8e8362e9b1217b53
yuki komatsu <yuki.k.lion@gmail.com>
2021-05-03 10:38:41 +0900

    fix design

--- components/top/AboutAiesec.vue
+++ components/top/AboutAiesec.vue
@@ -6,21 +6,21 @@
     </p>
     <div class="about-contents">
       <div class="about-content">
-        <img src="" alt="">
+        <img alt="" src="">
         <p>
-          <span>114の国と地域</span>
+          <span>100以上の国と地域</span>
           <span>の支部</span>
         </p>
       </div>
       <div class="about-content">
-        <img src="" alt="">
+        <img alt="" src="">
         <p>
           <span>7000以上の</span>
           <span>パートナー団体</span>
         </p>
       </div>
       <div class="about-content">
-        <img src="" alt="">
+        <img alt="" src="">
         <p>
           <span>年間30000件以上の</span>
           <span>経験を提供</span>
@@ -30,7 +30,7 @@
   </div>
 </template>
 
-<style scoped lang="scss">
+<style lang="scss" scoped>
 .about-container {
   min-height: 500px;
   display: flex;
@@ -39,40 +39,53 @@
   text-align: center;
   align-items: center;
 }
+
 .about-aiesec {
   color: $blue;
   font-size: 24px;
   padding-top: 100px;
   letter-spacing: 2px;
+  font-weight: bold;
+  font-style: italic;
 }
+
 .about-explain {
   font-size: 16px;
   letter-spacing: 2px;
+  line-height: 32px;
   padding-top: 50px;
   max-width: 800px;
+
   span {
     display: inline-block;
   }
 }
+
 .about-contents {
   display: flex;
   justify-content: center;
   flex-wrap: wrap;
   padding-top: 50px;
 }
+
 .about-content {
   display: flex;
   flex-direction: column;
   align-items: center;
   margin: 0 40px;
+
   img {
     padding: 100px;
     margin: 2em 0;
     background: $gray;
     border-radius: 10px;
   }
+
   p {
-    max-width: 140px;
+    max-width: 90%;
+    line-height: 32px;
+    font-weight: bold;
+
     span {
       display: inline-block;
     }
