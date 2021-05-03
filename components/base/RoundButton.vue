50b4e673a8270b4b6f28f49a0e2bfdd5cbcd0623
yuki komatsu <yuki.k.lion@gmail.com>
2021-05-03 16:55:48 +0900

    fix props extended

--- components/base/RoundButton.vue
+++ components/base/RoundButton.vue
@@ -1,18 +1,39 @@
 <template>
-  <button :style="{ width: width,color:color }">{{ name }}</button>
+  <button :class="classes" :style="style">{{ label }}</button>
 </template>
 
 <script>
 export default {
   props: {
-    name: String,
+    label: String,
     color: String,
-    width: String
-  }
+    backgroundColor: String || undefined,
+    size: {
+      type: String,
+      default: 'medium',
+      validator: function (value) {
+        return ['small', 'medium', 'large'].indexOf(value) !== -1;
+      },
+    },
+  },
+  computed: {
+    classes() {
+      return {
+        [`button-${this.size}`]: true,
+      };
+    },
+    style() {
+      return {
+        backgroundColor: this.backgroundColor || 'transparent',
+        color: this.color
+      };
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
+
 button {
   margin-top: 35px;
   margin-bottom: 140px;
@@ -23,5 +44,20 @@ button {
   font-weight: bold;
   font-size: 18px;
   letter-spacing: 2px;
+}
+
+.button {
+
+  &-small {
+    width: 10px;
+  }
+
+  &-medium {
+    width: 100px;
+  }
+
+  &-large {
+    width: 200px;
+  }
 }
 </style>
