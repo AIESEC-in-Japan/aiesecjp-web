5999caaba1ebb06aa2c04bc7a3d7972cc2067170
yuki komatsu <yuki.k.lion@gmail.com>
2021-05-04 15:37:21 +0900

    add safety page Top

---
+++ components/safety/SafetyDescriptions.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="safety-container">
+
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "SafetyDescriptions"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/safety/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="wrapper">
+    <SafetyTop/>
+    <SafetyDescriptions/>
+  </div>
+</template>
+
+<script>
+import SafetyTop from "@/components/safety/SafetyTop";
+import SafetyDescriptions from "@/components/safety/SafetyDescriptions";
+
+
+export default {
+  components: {SafetyDescriptions, SafetyTop}
+}
+</script>
