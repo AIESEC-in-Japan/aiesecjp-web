c52773dae56b0bd917cf34ea911d5adef7e67c3f
yuki komatsu <yuki.k.lion@gmail.com>
2021-05-04 15:58:48 +0900

    remove unused import

--- components/safety/SafetyDescriptions.vue
+++ components/safety/SafetyDescriptions.vue
@@ -1,5 +1,56 @@
 <template>
   <div class="safety-container">
+    <div class="safety-content">
+      <img src="~@/assets/images/safety/document.svg" alt="インターン生の皆様の安全管理サポートの体制" class="safety-content-icon">
+      <h2 class="safety-content-title">インターン生の皆様の安全管理サポートの体制</h2>
+      <p class="safety-content-description">
+        アイセック・ジャパンでは安全なインターンシッププログラムの継続的な運営を行うために、外部サービ スとも提携して海外安全管理を行っております。<br>
+        また、第三者の海外危機管理の専門家、有識者との定期的な海外安全管理委員会の開催なども行っており ます。
+      </p>
+      <div class="safety-content-items">
+        <h3 class="safety-content-items-title">海外危機管理専門の提携会社(安全サポート株式会社)</h3>
+        <p>海外危機管理が専門の提携会社がインターン生の24時間電話相談デスクや、インターン生の皆様の有事の 際のアドバイスを担当しております。提携企業の詳細は以下をご覧ください。 </p>
+        <a href="http://www.anzen-support.com/">→安全サポート株式会社HP(http://www.anzen-support.com/)</a>
+
+        <h3 class="safety-content-items-title"></h3>
+        <p></p>
+
+        <h3 class="safety-content-items-title"></h3>
+        <p></p>
+
+        <h3 class="safety-content-items-title"></h3>
+        <p></p>
+      </div>
+    </div>
+    <div class="safety-content">
+      <img src="~@/assets/images/safety/information.svg" alt="渡航前の安全管理サポート" class="safety-content-icon">
+      <h2 class="safety-content-title">渡航前の安全管理サポート</h2>
+      <p class="safety-content-description"></p>
+      <h3 class="safety-content-items-title"></h3>
+      <p></p>
+      <h3 class="safety-content-items-title"></h3>
+      <p></p>
+      <h3 class="safety-content-items-title"></h3>
+      <p></p>
+      <h3 class="safety-content-items-title"></h3>
+      <p></p>
+      <h3 class="safety-content-items-title"></h3>
+      <p></p>
+      <h3 class="safety-content-items-title"></h3>
+      <p></p>
+    </div>
+    <div class="safety-content">
+      <img src="~@/assets/images/safety/24-hours.svg" alt="渡航中の安全管理サポート" class="safety-content-icon">
+      <h2 class="safety-content-title">渡航中の安全管理サポート</h2>
+      <p class="safety-content-description"></p>
+      <h3 class="safety-content-items-title"></h3>
+      <p></p>
+      <h3 class="safety-content-items-title"></h3>
+      <p></p>
+      <h3 class="safety-content-items-title"></h3>
+      <p></p>
+
+    </div>
 
   </div>
 
--- pages/safety/index.vue
+++ pages/safety/index.vue
@@ -4,13 +4,3 @@
     <SafetyDescriptions/>
   </div>
 </template>
-
-<script>
-import SafetyTop from "@/components/safety/SafetyTop";
-import SafetyDescriptions from "@/components/safety/SafetyDescriptions";
-
-
-export default {
-  components: {SafetyDescriptions, SafetyTop}
-}
-</script>
