47119f1e1f6f155e2b4b7f2d05ffc3ed76d0a275
yuki komatsu <yuki.k.lion@gmail.com>
2021-05-09 19:48:24 +0900

    小難しいことをした場所にコメントを追記

--- plugins/scroll.js
+++ plugins/scroll.js
@@ -1,5 +1,11 @@
 import Vue from 'vue'
 
+// 今は使わないかもしれないけど、
+// スクロール量によってアニメーションが開始されるというイケてるデザインを取り入れたい時に使えるものです。
+// 書き方は公式のドキュメンとにあるものとほぼ同じなので使い方がわからない場合は以下を見てください。
+// https://vuejs.org/v2/cookbook/creating-custom-scroll-directives.html#v3-banner
+// by komatsu
+
 Vue.directive('scroll', {
   bind(el, binding, vnode) {
     let f = function (evt) {
