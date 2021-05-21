639586f885dc92db860a368e0e7a136482138332
Hikaru <caprice.dolce@gmail.com>
2021-05-21 14:04:51 +0900

    課題してみましたがまだまだうまくいきませんーー

---
+++ components/about/kadai.vue
@@ -0,0 +1,63 @@
+<template>
+<div class="kadai-container">
+  <div class="kadai-items">
+    <h2>MCBCXPメンバー一覧</h2>
+    <table class="kadai-list">
+      <tr>
+        <th>統括</th>
+        <td>出口啓悟</td>
+      </tr>
+      <tr>
+        <th></th>
+        <td>山地和花</td>
+      </tr>
+      <tr>
+        <th></th>
+        <td>後藤ひかる</td>
+      </tr>
+      <tr>
+        <th></th>
+        <td>霜田翔平</td>
+      </tr>
+      <tr>
+        <th></th>
+        <td>岡本大暉</td>
+      </tr>
+    </table>
+  </div>
+</div>
+</template>
+
+<style scoped lang="scss">
+.kadai-container {
+  width: 100%;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  margin: 50px auto;
+  h2 {
+    color: $blue;
+    font-size: 24px;
+    font-weight: bold;
+    letter-spacing: 2px;
+  }
+}
+.kadai-list {
+  font-size: 16px;
+  letter-spacing: 2px;
+  margin-top: 30px;
+  th, td {
+    padding: 24px 16px;
+    border-bottom-style: solid; 
+    border-bottom-width: 0.5px;
+    border-bottom-color: $gray;
+  }
+  th {
+    text-align: left;
+  }
+  td  {
+    padding-right: 160px;
+    padding-left: 50px;
+  }
+}
+</style>
