cbf5df207050bff8cd2bc7dc142c71f10e18cd81
yuki komatsu <yuki.k.lion@gmail.com>
2021-05-29 15:03:27 +0900

    add eslint settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,20 +2,23 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
+    "es2021": true
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: 'babel-eslint',
+    "ecmaVersion": 12,
+    "sourceType": "module"
   },
   extends: [
-    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+    // see this https://eslint.vuejs.org/user-guide/#usage
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential'
+    'plugin:vue/strongly-recommended',
+    "eslint:recommended",
   ],
-  // required to lint *.vue files
   plugins: [
     'vue'
   ],
-  // add your custom rules here
   rules: {}
 }
+
