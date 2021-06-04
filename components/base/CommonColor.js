1d4c482884a4da9fad95ff9e79fc3a26949c9c2b
yuki komatsu <yuki.k.lion@gmail.com>
2021-06-05 01:05:40 +0900

    extract button

---
+++ components/base/CommonColor.js
@@ -0,0 +1,12 @@
+export const basic_color = {
+  'blue': '#037Ef3',
+  'light-gray': '#F3F4F7',
+  'gray': '#CACCD1',
+  'dark-gray': '#52565E',
+  'yellow': '#FFC845',
+  'orange': '#F48924',
+  'red': '#F85A40',
+  'green': '#00C16E',
+  'mint': '#0CB9C1',
+  'white': 'white'
+}
---
+++ components/base/RoundButton.vue
@@ -0,0 +1,89 @@
+<template>
+  <button
+    :class="classes"
+    :style="style"
+    v-on:mouseleave="mouseLeaveAction"
+    v-on:mouseover="mouseOverAction"
+  >
+    {{ label }}
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    label: String,
+    textColor: String,
+    baseColor: String || undefined,
+    size: {
+      type: String,
+      default: 'medium',
+      validator: function (value) {
+        return ['small', 'medium', 'large'].indexOf(value) !== -1;
+      },
+    },
+  },
+  data() {
+    return {
+      isHover: false
+    }
+  },
+  methods: {
+    mouseOverAction() {
+      this.isHover = true;
+    },
+    mouseLeaveAction() {
+      this.isHover = false;
+    }
+  },
+  computed: {
+    classes() {
+      return {
+        [`button-${this.size}`]: true,
+      };
+    },
+    style() {
+      return {
+        backgroundColor: this.computedBaseColor,
+        color: this.computedTextColor
+      };
+    },
+    computedBaseColor() {
+      if (this.baseColor === null) {
+        return this.isHover ? this.textColor : 'transparent'
+      }
+      return this.isHover ? this.textColor : this.baseColor
+    },
+    computedTextColor() {
+      if (this.baseColor === null) {
+        return this.isHover ? '#fff' : this.textColor
+      }
+      return this.isHover ? this.baseColor : this.textColor
+    }
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+button {
+  padding: 15px;
+  background-color: transparent;
+  border-radius: 40px;
+  border: solid 2px;
+  font-weight: bold;
+  font-size: 18px;
+  letter-spacing: 2px;
+}
+.button {
+  // 今のところ使い道はなさそうだけど、sample codeの役割で追加
+  &-small {
+    width: 100px;
+  }
+  &-medium {
+    width: 200px;
+  }
+  &-large {
+    width: 400px;
+  }
+}
+</style>
