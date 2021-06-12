e0e48722965dac6a44d1949d0782088d836e39f5
Gakuto Miyauchi <gakuto7733@gmail.com>
2021-06-12 17:28:05 +0900

    コンポーネントとして切り出し

---
+++ components/base/IconWithSrc.vue
@@ -0,0 +1,26 @@
+<template>
+    <img 
+        :src="src"
+        :alt="alt"
+        :url="url"
+    >
+</template>
+
+<script>
+export default {
+    props: {
+        src: {
+            type: String,
+            required: true
+        },
+        alt: {
+            type: String,
+            required: false
+        },
+        url: {
+            type: String,
+            required: false
+        }
+    }
+}
+</script>
--- components/top/OurPartners.vue
+++ components/top/OurPartners.vue
@@ -1,90 +1,81 @@
 <template>
   <div class="partners-container">
     <h2>Our Partners</h2>
-    <CorporateLogo 
-    />
-    <!-- <div class="partners-contents">
-      <img src="@/assets/2020corporation-logo/accenture.png" alt="" class="partners-content">
-      <img src="@/assets/2020corporation-logo/enjapan.png" alt="" class="partners-content">
-      <img src="@/assets/2020corporation-logo/endroll.jpg" alt="" class="partners-content">
-      <img src="@/assets/2020corporation-logo/オースビー.png" alt="" class="partners-content">
-      <img src="@/assets/2020corporation-logo/gaiax.png" alt="" class="partners-content">
-      <img src="@/assets/2020corporation-logo/givery.jpg" alt="" class="partners-content">
-      <img src="@/assets/2020corporation-logo/sairu.jpg" alt="" class="partners-content">
-      <img src="@/assets/2020corporation-logo/sansan.png" alt="" class="partners-content">
-      <img src="@/assets/2020corporation-logo/sunstar.jpg" alt="" class="partners-content">
-      <img src="@/assets/2020corporation-logo/cuc.png" alt="" class="partners-content">
-      <img src="@/assets/2020corporation-logo/schneiderelectric.png" alt="" class="partners-content">
-      <img src="@/assets/2020corporation-logo/johnsonandjohnson.png" alt="" class="partners-content">
-      <img src="@/assets/2020corporation-logo/spartaenglishmethod.png" alt="" class="partners-content">
-      <img src="@/assets/2020corporation-logo/softbank.png" alt="" class="partners-content">
-      <img src="@/assets/2020corporation-logo/dentsu.gif" alt="" class="partners-content">
-      <img src="@/assets/2020corporation-logo/toyobo.svg" alt="" class="partners-content">
-      <img src="@/assets/2020corporation-logo/nissan.png" alt="" class="partners-content">
-      <img src="@/assets/2020corporation-logo/nissha.png" alt="" class="partners-content">
-      <img src="@/assets/2020corporation-logo/panasonic.png" alt="" class="partners-content">
-      <img src="@/assets/2020corporation-logo/prtable.png" alt="" class="partners-content">
-      <img src="@/assets/2020corporation-logo/bizreach.png" alt="" class="partners-content">
-      <img src="@/assets/2020corporation-logo/florence.jpg" alt="" class="partners-content">
-      <img src="@/assets/2020corporation-logo/mizuho.jpg" alt="" class="partners-content">
-      <img src="@/assets/2020corporation-logo/langoo.jpg" alt="" class="partners-content">
-      <img src="@/assets/2020corporation-logo/lifull.jpg" alt="" class="partners-content">
-      <img src="@/assets/2020corporation-logo/RIZAP.png" alt="" class="partners-content">
-      <img src="@/assets/2020corporation-logo/realworld.png" alt="" class="partners-content">
-      <img src="@/assets/2020corporation-logo/リクルートマネジメントソリューションズ.jpg" alt="" class="partners-content">
-      <img src="@/assets/2020corporation-logo/legaseed.jpg" alt="" class="partners-content">
-      <img src="@/assets/2020corporation-logo/wacul.png" alt="" class="partners-content">
-    </div> -->
+    <div class="partners-contents" >
+      <span v-for="logo in logos" :key="logo.src" >
+        <IconWithSrc 
+          v-bind:src="logo.src"
+          :alt="logo.alt"
+          class="partners-content"
+        />
+      </span>
+    </div>
   </div>
 </template>
 
 <script>
-// const corporatelogo = [
-//   {name: accenture.png, alt: ""},
-//   {name: enjapan.png, alt: ""},
-//   {name: endroll.jpg, alt: ""},
-//   {name: オースビー.png, alt: ""},
-//   {name: gaiax.png, alt: ""},
-// ]
-
-
-
-
+export default {
+    data() {
+        return {
+            logos: [
+                { src: "_nuxt/assets/2020corporation-logo/accenture.png", alt: "アクセンチュア株式会社のロゴ"},
+                { src: "_nuxt//assets/2020corporation-logo/enjapan.png", alt: "エン・ジャパン株式会社のロゴ"},
+                { src: "_nuxt/assets/2020corporation-logo/endroll.jpg", alt: "株式会社エンドロールのロゴ"},
+                { src: "_nuxt/assets/2020corporation-logo/オースビー.png", alt: "株式会社オースビーのロゴ"},
+                { src: "_nuxt/assets/2020corporation-logo/gaiax.png", alt: "株式会社ガイアックスのロゴ"},
+                { src: "_nuxt/assets/2020corporation-logo/givery.jpg", alt: "株式会社ギブリーのロゴ"},
+                { src: "_nuxt/assets/2020corporation-logo/sairu.jpg", alt: "株式会社才流のロゴ"},
+                { src: "_nuxt/assets/2020corporation-logo/sansan.png", alt: "Sansan株式会社のロゴ"},
+                { src: "_nuxt/assets/2020corporation-logo/sunstar.jpg", alt: "サンスター株式会社のロゴ"},
+                { src: "_nuxt/assets/2020corporation-logo/cuc.png", alt: "株式会社シーユーシーのロゴ"},
+                { src: "_nuxt/assets/2020corporation-logo/schneiderelectric.png", alt: "シュナイダーエレクトリック株式会社のロゴ"},
+                { src: "_nuxt/assets/2020corporation-logo/johnsonandjohnson.png", alt: "ジョンソン・エンド・ジョンソン株式会社のロゴ"},
+                { src: "_nuxt/assets/2020corporation-logo/spartaenglishmethod.png", alt: "株式会社スパルタ英会話のロゴ"},
+                { src: "_nuxt/assets/2020corporation-logo/softbank.png", alt: "ソフトバンク株式会社のロゴ"},
+                { src: "_nuxt/assets/2020corporation-logo/dentsu.gif", alt: "株式会社電通のロゴ"},
+                { src: "_nuxt/assets/2020corporation-logo/toyobo.svg", alt: "東洋紡株式会社のロゴ"},
+                { src: "_nuxt/assets/2020corporation-logo/nissan.png", alt: "日産自動車株式会社のロゴ"},
+                { src: "_nuxt/assets/2020corporation-logo/nissha.png", alt: "NISSHA株式会社のロゴ"},
+                { src: "_nuxt/assets/2020corporation-logo/panasonic.png", alt: "パナソニック株式会社のロゴ"},
+                { src: "_nuxt/assets/2020corporation-logo/prtable.png", alt: "株式会社PR Table のロゴ"},
+                { src: "_nuxt/assets/2020corporation-logo/bizreach.png", alt: "株式会社ビズリーチのロゴ"},
+                { src: "_nuxt/assets/2020corporation-logo/florence.jpg", alt: "認定NPO法人フローレンスのロゴ"},
+                { src: "_nuxt/assets/2020corporation-logo/mizuho.jpg", alt: "ミズホ株式会社のロゴ"},
+                { src: "_nuxt/assets/2020corporation-logo/langoo.jpg", alt: "株式会社langooのロゴ"},
+                { src: "_nuxt/assets/2020corporation-logo/lifull.jpg", alt: "株式会社LIFULLのロゴ"},
+                { src: "_nuxt/assets/2020corporation-logo/RIZAP.png", alt: "RIZAPグループ株式会社のロゴ"},
+                { src: "_nuxt/assets/2020corporation-logo/リクルートマネジメントソリューションズ.jpg", alt: "株式会社リクルートマネジメントソリューションズのロゴ"},
+                { src: "_nuxt/assets/2020corporation-logo/legaseed.jpg", alt: "株式会社Legaseedのロゴ"},
+                { src: "_nuxt/assets/2020corporation-logo/wacul.png", alt: "株式会社WACULのロゴ"},
+            ]
+        }
+    }
+};
 </script>
-
-
 
 <style scoped lang="scss">
 .partners-container {
-  min-height: 500px;
   width: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
   h2 {
     color: $blue;
-    font-size: 24px;
+    font-size: 1.5rem;
     letter-spacing: 2px;
-    padding-top: 150px;
+    padding-top: 10rem;
   }
 }
 .partners-contents {
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
-  padding-top: 75px;
+  padding-top: 2rem;
 }
 .partners-content {
-  width: 120px;
-  height: 120px;
-  margin: 30px;
+  width: 7.5rem;
+  height: 7.5rem;
+  margin: 2rem;
   object-fit: contain;
-}
-.partners-content-empty {
-  width: 200px;
-  height: 0;
-  padding-top: 0;
-  padding-bottom: 0;
-  margin: 0 30px 0;
 }
 </style>
