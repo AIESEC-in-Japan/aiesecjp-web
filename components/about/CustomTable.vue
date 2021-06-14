763b0ec751ef943e6c7270411d9c6a93acd6846e
Hikaru <caprice.dolce@gmail.com>
2021-06-14 21:15:05 +0900

    tableの書き出し

---
+++ components/about/CustomTable.vue
@@ -0,0 +1,60 @@
+<template>
+    <table class='officer-table'>
+    <tr>
+        <th>{{position}}</th>
+        <td>{{name}}</td>
+    </tr>
+    </table>
+
+</template>
+
+<script>
+export default {
+    props:{
+        position: String,
+        name: String
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+table{
+    width: 800px;
+    font-size: 16px;
+    letter-spacing: 2px;
+    margin-top: 30px;
+}
+
+th, td{
+    border-bottom-style: solid; 
+    border-bottom-width: 0.5px;
+    border-bottom-color: $gray;
+    padding: 24px 16px;
+}
+
+th{
+    width: 108px;
+    text-align: left;
+}
+
+td{
+    line-height: 21px;
+}
+
+.aboutus-table{
+    &-container{
+        width: 800px;
+        margin: 50px auto;
+      }
+    
+    &-title{
+        color: $blue;
+        font-size: 24px;
+        font-weight: bold;
+        letter-spacing: 2px;
+        margin-top: 40px;
+        margin-bottom: 20px;
+      }
+}
+
+</style>
