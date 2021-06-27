7647131c7e1ea190067d34ea15a98c04a7957cd8
yuki komatsu <yuki.k.lion@gmail.com>
2021-06-27 17:20:35 +0900

    add voice section

---
+++ components/global-youth-dialogue/Voice.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="global-youth-dialogue-voice">
+    <BaseRoundButton
+      v-bind="this.ButtonProps"
+      :label="'参加者の声はこちら'"
+      @click="window.open('https://note.com/aiesec_japan/n/n507bb45b442a')"
+    />
+  </div>
+</template>
+
+<script>
+import {basic_color} from "@/components/base/CommonColor";
+
+export default {
+  data() {
+    return {
+      ButtonProps: {
+        textColor: basic_color.white,
+        baseColor: basic_color.deepBlue,
+        size: 'large'
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.global-youth-dialogue-voice {
+  width: 80%;
+  margin: 0 auto;
+  text-align: center;
+}
+</style>
