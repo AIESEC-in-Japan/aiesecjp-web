2f0d0d8b8bc38c8c7c5f31112ffc78a21848b50f
yuki komatsu <yuki.k.lion@gmail.com>
2021-07-02 23:37:01 +0900

    rwd

--- components/contact-us/Form.vue
+++ components/contact-us/Form.vue
@@ -1,15 +1,17 @@
 <template>
   <iframe
-    height="1331"
-    src="https://docs.google.com/forms/d/e/1FAIpQLSfn-kinSEaa8mTjjxsOaguAbIEL-W5eo1sLSexfC-RftIcYRQ/viewform?embedded=true"
-    width="640"
     class="google-form"
+    src="https://docs.google.com/forms/d/e/1FAIpQLSfn-kinSEaa8mTjjxsOaguAbIEL-W5eo1sLSexfC-RftIcYRQ/viewform?embedded=true"
   />
 </template>
 
 
-<style scoped lang="scss">
-.google-form{
+<style lang="scss" scoped>
+.google-form {
   border-width: 0;
+  height: 100rem;
+  max-width: 40rem;
+  width: 90%;
+  margin: 7rem auto;
 }
 </style>
