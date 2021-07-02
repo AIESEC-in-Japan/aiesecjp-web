98fb2a5bc4bd0bff2d0740c328be9642e5e50d60
yuki komatsu <yuki.k.lion@gmail.com>
2021-07-02 13:45:02 +0900

    add form

---
+++ components/contact-us/Form.vue
@@ -0,0 +1,15 @@
+<template>
+  <iframe
+    height="1331"
+    src="https://docs.google.com/forms/d/e/1FAIpQLSfn-kinSEaa8mTjjxsOaguAbIEL-W5eo1sLSexfC-RftIcYRQ/viewform?embedded=true"
+    width="640"
+    class="google-form"
+  />
+</template>
+
+
+<style scoped lang="scss">
+.google-form{
+  border-width: 0;
+}
+</style>
