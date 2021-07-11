0227bc27eebe072267aeadb5a4e47eb9d7090823
yuki komatsu <yuki.k.lion@gmail.com>
2021-07-11 19:57:05 +0900

    footerが被って見えないので修正

--- components/global-youth-dialogue/FixedFooter.vue
+++ components/global-youth-dialogue/FixedFooter.vue
@@ -1,25 +1,21 @@
 <template>
   <!--  少しずつ便利な仕組みを導入してデザインを整えていく -->
-  <client-only>
-    <transition name="fade">
-      <v-footer
-        v-show="!isBottom"
-        fixed
-        height="80"
-        padless
-      >
-        <v-row
-          align="center"
-          justify="center"
-        >
-          <BaseRoundButton
-            v-bind="this.ButtonProps"
-            :label="'公式LINEから最新情報をチェック!'"
-          />
-        </v-row>
-      </v-footer>
-    </transition>
-  </client-only>
+  <v-footer
+    v-show="!isBottom"
+    fixed
+    height="80"
+    padless
+  >
+    <v-row
+      align="center"
+      justify="center"
+    >
+      <BaseRoundButton
+        v-bind="this.ButtonProps"
+        :label="'公式LINEから最新情報をチェック!'"
+      />
+    </v-row>
+  </v-footer>
 </template>
 
 
@@ -39,14 +35,10 @@ export default {
   },
   computed: {
     isBottom() {
-      console.log("process", process.browser)
-      console.log("$Y",this.$window.pageYOffset)
-      console.log("gY",global.pageYOffset)
-      console.log("ev",this.$window.pageYOffset > 1000);
+      // window.documentがSSRでは取れないのでブラウザのみで評価する
+      // 本当は消えるアニメーション入れたいけどうまくいかなかったので後回し
+      // 1000は、footerサイズを考慮して適当書いたので適宜変更してください
       if (process.browser) {
-        console.log("global", global);
-        console.log("if height",global.window.document.body.scrollHeight);
-        console.log("if ev",this.$window.pageYOffset > global.window.document.body.scrollHeight - 1000);
         return this.$window.pageYOffset > global.window.document.body.scrollHeight - 1000;
       }
       return false;
