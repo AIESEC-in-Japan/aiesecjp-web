94cff8dea04ad224d968c28be57d583039fec198
yuki komatsu <yuki.k.lion@gmail.com>
2021-07-11 19:27:41 +0900

    実験中のやつ残しておきます

--- components/global-youth-dialogue/FixedFooter.vue
+++ components/global-youth-dialogue/FixedFooter.vue
@@ -1,20 +1,25 @@
 <template>
   <!--  少しずつ便利な仕組みを導入してデザインを整えていく -->
-  <v-footer
-    fixed
-    height="80"
-    padless
-  >
-    <v-row
-      align="center"
-      justify="center"
-    >
-      <BaseRoundButton
-        v-bind="this.ButtonProps"
-        :label="'公式LINEから最新情報をチェック!'"
-      />
-    </v-row>
-  </v-footer>
+  <client-only>
+    <transition name="fade">
+      <v-footer
+        v-show="!isBottom"
+        fixed
+        height="80"
+        padless
+      >
+        <v-row
+          align="center"
+          justify="center"
+        >
+          <BaseRoundButton
+            v-bind="this.ButtonProps"
+            :label="'公式LINEから最新情報をチェック!'"
+          />
+        </v-row>
+      </v-footer>
+    </transition>
+  </client-only>
 </template>
 
 
@@ -30,6 +35,21 @@ export default {
         size: 'large',
         url: 'https://lin.ee/GaZkjsN'
       }
+    }
+  },
+  computed: {
+    isBottom() {
+      console.log("process", process.browser)
+      console.log("$Y",this.$window.pageYOffset)
+      console.log("gY",global.pageYOffset)
+      console.log("ev",this.$window.pageYOffset > 1000);
+      if (process.browser) {
+        console.log("global", global);
+        console.log("if height",global.window.document.body.scrollHeight);
+        console.log("if ev",this.$window.pageYOffset > global.window.document.body.scrollHeight - 1000);
+        return this.$window.pageYOffset > global.window.document.body.scrollHeight - 1000;
+      }
+      return false;
     }
   }
 }
