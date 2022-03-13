65a64f3fd135adf3b10d8174f8634afded2fa5c4
yuki komatsu <yuki.k.lion@gmail.com>
2022-03-13 22:34:09 +0900

    vueのstorybookの書き方がよくわからなかったので、一旦コメントアウトしておきます

--- components/base/RoundButton.stories.js
+++ components/base/RoundButton.stories.js
@@ -1,21 +1,21 @@
-import RoundButton from './RoundButton.vue';
-import {basic_color} from "@/components/base/CommonColor";
-
-export default {
-  title: 'components/base/RoundButton',
-  component: RoundButton,
-};
-
-export const RoundButtonStory = () => ({
-  components: {RoundButton},
-  template: '<RoundButton v-bind="buttonProps" />',
-});
-
-
-const buttonProps = {
-  textColor: basic_color.deepBlue,
-  baseColor: 'transparent',
-  size: 'large',
-  url: 'https://lin.ee/GaZkjsN',
-  label: "this is button"
-}
+// import RoundButton from './RoundButton.vue';
+// import {basic_color} from "@/components/base/CommonColor";
+//
+// export default {
+//   title: 'components/base/RoundButton',
+//   component: RoundButton,
+// };
+//
+// export const RoundButtonStory = () => ({
+//   components: {RoundButton},
+//   template: '<RoundButton v-bind=`buttonProps` />',
+// });
+//
+//
+// const buttonProps = {
+//   textColor: basic_color.deepBlue,
+//   baseColor: 'transparent',
+//   size: 'large',
+//   url: 'https://lin.ee/GaZkjsN',
+//   label: "this is button"
+// }
