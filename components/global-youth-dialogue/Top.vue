40bd504439b41153c5039bfec9abbfbcc7f8be98
NaoE241 <asunaro@gmail.com>
2022-04-23 22:33:36 +0900

    set ga-tag on gyd

--- components/global-youth-dialogue/Top.vue
+++ components/global-youth-dialogue/Top.vue
@@ -1,3 +1,13 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-Q12106X7XB"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-Q12106X7XB');
+</script>
+
 <template>
   <section class="global-youth-dialogue-container">
     <div class="global-youth-dialogue-wrapper">
