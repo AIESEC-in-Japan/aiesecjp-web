79fdc4224d507f0f514d32a6d22e22878259589e
dddaiki <97176797+dddaiki-geek@users.noreply.github.com>
2022-04-26 21:36:04 +0900

    tab

--- pages/global-youth-dialogue/index.vue
+++ pages/global-youth-dialogue/index.vue
@@ -9,28 +9,28 @@
 </template>
 
 <script>
-export default {
-  head() {
-    return{
-      title: 'aiesecjp-web',
-      meta: [
-        {charset: 'utf-8'},
-        {name: 'viewport', content: 'width=device-width, initial-scale=1'},
-        {hid: 'description', name: 'description', content: 'AIESEC in Japan official website'}
-      ],
-      link: [
-        {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
-      ],
-      script: [{
-        async: true,
-        src: ‘https: //www.googletagmanager.com/gtag/js?id=G-F3F77C5HPD’ 
-        },
-        {
-          innerHTML: `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag(‘js’, new Date());gtag(‘config’, ‘G-F3F77C5HPD’);`
-        }
-      ]
-    } 
+  export default {
+    head() {
+      return{
+        title: 'aiesecjp-web',
+        meta: [
+          {charset: 'utf-8'},
+          {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+          {hid: 'description', name: 'description', content: 'AIESEC in Japan official website'}
+        ],
+        link: [
+          {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
+        ],
+        script: [{
+          async: true,
+          src: ‘https: //www.googletagmanager.com/gtag/js?id=G-F3F77C5HPD’ 
+          },
+          {
+            innerHTML: `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag(‘js’, new Date());gtag(‘config’, ‘G-F3F77C5HPD’);`
+          }
+        ]
+      } 
+    }
   }
-}
 </script>
 
