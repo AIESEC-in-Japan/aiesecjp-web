31775c367321877cce06fe0b3593b25abc261840
sashimin <ishiimm26@gmail.com>
2023-10-23 23:49:55 +0900

    update:

--- static/lp/outgoing/assets/js/script.js
+++ static/lp/outgoing/assets/js/script.js
@@ -4,7 +4,6 @@ $(function(){
     modal();
     faq();
     GethashID();
-    worksSlider();
   })
 //ジャンプ
 function smoothScroll(){
@@ -13,10 +12,13 @@ function smoothScroll(){
       let href= $(this).attr("href");
       let target = $(href == "#" || href == "" ? 'html' : href);
       let position = 0;
+      let w = screen.width;
       if(target.length > 0){
         position = target.offset().top - 100;
-        if(href == "#about"){
-            position += 350
+        if(href =="#about"){
+          if(w > 480){
+            position += 130
+          }
         }
       }
       if(!($(this).hasClass("selected"))){
@@ -52,35 +54,33 @@ function faq(){
 }
 
 //タブメニュー
-//任意のタブにURLからリンクするための設定
 function GethashID (hashIDName){
 	if(hashIDName){
-		//タブ設定
-		$('.tab li').find('a').each(function() { //タブ内のaタグ全てを取得
-			var idName = $(this).attr('href'); //タブ内のaタグのリンク名（例）#lunchの値を取得
-			if(idName == hashIDName){ //リンク元の指定されたURLのハッシュタグ（例）http://example.com/#lunch←この#の値とタブ内のリンク名（例）#lunchが同じかをチェック
-				var parentElm = $(this).parent(); //タブ内のaタグの親要素（li）を取得
-				$('.tab li').removeClass("active"); //タブ内のliについているactiveクラスを取り除き
-				$(parentElm).addClass("active"); //リンク元の指定されたURLのハッシュタグとタブ内のリンク名が同じであれば、liにactiveクラスを追加
+		$('.tab li').find('a').each(function() {
+			var idName = $(this).attr('href'); 
+			if(idName == hashIDName){ 
+				var parentElm = $(this).parent();
+				$('.tab li').removeClass("active");
+				$(parentElm).addClass("active");
 				//表示させるエリア設定
-				$('.area').removeClass("is-active"); //もともとついているis-activeクラスを取り除き
-				$(hashIDName).addClass("is-active"); //表示させたいエリアのタブリンク名をクリックしたら、表示エリアにis-activeクラスを追加	
+				$('.area').removeClass("is-active");
+				$(hashIDName).addClass("is-active");
 			}
 		});
 	}
 }
 //タブをクリックしたら
 $('.tab a').on('click', function() {
-	var idName = $(this).attr('href'); //タブ内のリンク名を取得	
-	GethashID (idName);//設定したタブの読み込みと
-	return false;//aタグを無効にする
+	var idName = $(this).attr('href');	
+	GethashID (idName);
+	return false;
 });
 // 上記の動きをページが読み込まれたらすぐに動かす
 $(window).on('load', function () {
-    $('.tab li:first-of-type').addClass("active"); //最初のliにactiveクラスを追加
-    $('.area:first-of-type').addClass("is-active"); //最初の.areaにis-activeクラスを追加
-	var hashName = location.hash; //リンク元の指定されたURLのハッシュタグを取得
-	GethashID (hashName);//設定したタブの読み込み
+    $('.tab li:first-of-type').addClass("active");
+    $('.area:first-of-type').addClass("is-active"); 
+	var hashName = location.hash; 
+	GethashID (hashName);
 });
 
 //ハンバーガーメニュー
@@ -95,17 +95,12 @@ function headerSpMenu(){
       $('#js-header_content').addClass('is-show');
       $('#js-header_logo').addClass('is-white');
     }
-  })
-}
+  });
 
-//スライダー
-function worksSlider(){
-  const worksSlider = new Swiper('.js-swiper--works',{
-    speed: 400,
-    autoplay:true,
-    loop: true,
-    slidesPerView: 1.5,
-    centeredSlides: true,
-    centeredSlidesBounds: true,
+  $('#menu a[href]').on('click', function(event) {
+    let w = screen.width;
+    if(w <= 480){
+      $('.header-hamburger').trigger('click');
+    }
   });
 }
