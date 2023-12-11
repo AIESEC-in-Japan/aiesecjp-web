4729aef2d19c7b811e9df2876b662290a174b280
sashimin <ishiimm26@gmail.com>
2023-12-11 18:11:02 +0900

    update: Facebook Metaタグ一時停止 (cookie許可機能導入に伴い未対応のため)

--- pages/global-youth-dialogue/index.vue
+++ pages/global-youth-dialogue/index.vue
@@ -33,18 +33,20 @@
           },
           {
             innerHTML: `window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-KTWNHJF2EJ');`
-          },
-          // Meta Pixel Code
-          {
-            innerHTML: `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window, document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init', '441541344740997');fbq('track', 'PageView');`
           }
-        ],
-        __dangerouslyDisableSanitizers: ['script'],
-        noscript: [
-          {
-            innerHTML: `<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=441541344740997&ev=PageView&noscript=1"/>`,
-          }
-        ],
+          // , cookie許可機能導入に伴い未対応のため、Facebook Metaタグ一時停止
+          // // Meta Pixel Code
+          // {
+          //   innerHTML: `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window, document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init', '441541344740997');fbq('track', 'PageView');`
+          // }
+        ]
+        // ,
+        // __dangerouslyDisableSanitizers: ['script'],
+        // noscript: [
+        //   {
+        //     innerHTML: `<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=441541344740997&ev=PageView&noscript=1"/>`,
+        //   }
+        // ],
       }
     }
   }
