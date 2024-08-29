116447c475c9bf49ea46457ab323eaecb2e81af9
sashimin <ishiimm26@gmail.com>
2024-08-29 21:42:02 +0900

    add: サイトポリシーページの追加

---
+++ components/site-policy/Top.stories.js
@@ -0,0 +1,11 @@
+import Top from './Top.vue';
+
+export default {
+  title: 'components/site-policy/Top',
+  component: Top,
+};
+
+export const TopStory = () => ({
+  components: {Top},
+  template: '<Top />',
+});
---
+++ pages/site-policy/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="wrapper">
+    <SitePolicyTop />
+    <SitePolicyDescriptions />
+  </div>
+</template>
